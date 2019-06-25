e50ba347e1c5d30dea6af71de69d6bf7cc8e7a77
Phillip Booker <phillippbooker@gmail.com>
2019-06-24 18:45:47 -0700

    Using auto fix method to fix lint issue.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,8 +17,9 @@ module.exports = function(app) {
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    // eslint-disable-next-line prettier/prettier
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.json(dbExample);
     });
   });
