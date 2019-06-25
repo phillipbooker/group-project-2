6d39326c26a625a48d56505e615fcf2a33c40b4a
Phillip Booker <phillippbooker@gmail.com>
2019-06-24 18:38:42 -0700

    Attempts to ignore for line causing prettier error.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,9 +17,8 @@ module.exports = function(app) {
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({
-      where: { id: req.params.id }
-    }).then(function(dbExample) {
+    // eslint-disable-next-line prettier/prettier
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
       res.json(dbExample);
     });
   });
