28ff13dbca37f2e231897193dbb814f355ff2e04
rasenin <romansenin15@gmail.com>
2019-06-24 22:04:39 -0700

    set up what should make auth work

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 require("dotenv").config();
+
+// Globals
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const GoogleStrategy = require("passport-google-oauth20");
 var express = require("express");
 var exphbs = require("express-handlebars");
 
@@ -33,34 +38,16 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
-});
-
-module.exports = app;
-
-// Globals
-const passport = require("passport");
-const cookieSession = require("cookie-session");
-const GoogleStrategy = require("passport-google-oauth20");
-
 // Google login credentials, used when the user contacts
 // Google, to tell them where he is trying to login to, and show
 // that this domain is registered for this service.
 // Google will respond with a key we can use to retrieve profile
 // information, packed into a redirect response that redirects to
-// server162.site:[port]/auth/redirect
+// https://[our-app-name]/auth/redirect
 const googleLoginData = {
   clientID:
-    "535358974554-clfobhvtlf8sdsnoac36i4bl6cv8n092.apps.googleusercontent.com",
-  clientSecret: "upkyj1pDCqWLnYtMKIIfxJmG",
+    "270805595988-eg9psoa21hts52ohjh57mgdo7s4tjch0.apps.googleusercontent.com",
+  clientSecret: "BoV6sGVm0bfb8rgV9DG_Yosc",
   callbackURL: "/auth/redirect"
 };
 
@@ -71,8 +58,6 @@ const googleLoginData = {
 // to Google for the user's profile information.
 // It will get used much later in the pipeline.
 passport.use(new GoogleStrategy(googleLoginData, gotProfile));
-
-// Let's build a server pipeline!
 
 // pipeline stage that just echos url, for debugging
 app.use("/", printURL);
@@ -94,9 +79,6 @@ app.use(passport.initialize());
 // If there is a valid cookie, will call deserializeUser()
 app.use(passport.session());
 
-// Public static files
-app.get("/*", express.static("public"));
-
 // next, handler for url that starts login with Google.
 // The app (in public/login.html) redirects to here (not an AJAX request!)
 // Kicks off login process by telling Browser to redirect to
@@ -116,7 +98,7 @@ app.get(
   "/auth/redirect",
   // for educational purposes
   function(req, res, next) {
-    // console.log("at auth/redirect");
+    console.log("at auth/redirect");
     next();
   },
   // This will issue Server's own HTTPS request to Google
@@ -128,18 +110,7 @@ app.get(
   // will come back here to send back the response
   // ...with a cookie in it for the Browser!
   function(req, res) {
-    // console.log("Logged in and using cookies!");
-
-    res.redirect("/user/index.html");
-  }
-);
-
-app.get(
-  "/",
-  isAuthenticated, // only pass on to following function if
-  // user is logged in
-  // serving files that start with /user from here gets them from ./
-  function(req, res) {
+    console.log("Logged in and using cookies!");
     res.redirect("/user/index.html");
   }
 );
@@ -153,88 +124,14 @@ app.get(
   express.static(".")
 );
 
-app.get("/translate", getTranslation);
-
-app.post("/store", (req, res) => {
-  let q = req.query;
-  saveToDatabase(
-    req.user.userId,
-    q.source,
-    q.translated,
-    q.numShown,
-    q.numCorrect
-  );
-  res.send();
-});
-
-app.get("/review", (req, res) => {
-  getFromDatabase(req.query.id, (error, response) => {
-    if (error) {
-      console.log("THIS IS ERROR: ", error);
-      res.json(error);
-    } else {
-      // console.log("THIS IS RESPONSE: ", response);
-      res.json(response);
-    }
-  });
-});
-
-app.get("/allreviews", (req, res) => {
-  getAllFromDatabase(req.user.userId, (error, response) => {
-    if (error) {
-      console.log("THIS IS ERROR: ", error);
-      res.json(error);
-    } else {
-      // console.log("THIS IS RESPONSE: ", response);
-      res.json(response);
-    }
-  });
-});
-
-app.put("/store", (req, res) => {
-  let q = req.query;
-  updateNums(q.id, q.numShown, q.numCorrect, err => {
-    if (err) {
-      console.log(err);
-    }
-  });
-  res.send();
-});
-
-// gets which view to display for the user
-app.get("/getView", (req, res) => {
-  database.get(
-    `SELECT COUNT(*) FROM flashcards WHERE userId = ?`,
-    [req.user.userId.toString()],
-    (err, response) => {
-      if (err) throw err;
-      // console.log("count is: ", response["COUNT(*)"]);
-      if (response["COUNT(*)"] === 0) {
-        res.send("main");
-      } else {
-        res.send("review");
-      }
-    }
-  );
-});
-
-// gets user data to the App component
-app.get("/getData", (req, res) => {
-  res.send(req.user);
-});
-
 // finally, not found...applies to everything
 app.use(fileNotFound);
-
-app.listen(PORT, () => {
-  console.log("Listening on port " + PORT);
-});
 
 // middleware functions
 
 // print the url of incoming HTTP request
 function printURL(req, res, next) {
-  // console.log(req.url);
+  console.log(req.url);
   next();
 }
 
@@ -242,8 +139,8 @@ function printURL(req, res, next) {
 // personal data
 function isAuthenticated(req, res, next) {
   if (req.user) {
-    // console.log("Req.session:", req.session);
-    // console.log("Req.user:", req.user);
+    console.log("Req.session:", req.session);
+    console.log("Req.user:", req.user);
     next();
   } else {
     res.redirect("/login.html"); // send response telling
@@ -268,51 +165,18 @@ function fileNotFound(req, res) {
 // is called (in /auth/redirect/),
 // once we actually have the profile data from Google.
 function gotProfile(accessToken, refreshToken, profile, done) {
-  // console.log("Google profile", profile);
-
+  console.log("Google profile", profile);
   // here is a good place to check if user is in DB,
   // and to store him in DB if not already there.
   // Second arg to "done" will be passed into serializeUser,
   // should be key to get user out of database.
 
-  database.run(
-    `CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    userId INTEGER,
-    firstName TEXT,
-    lastName TEXT
-  );
-  `,
-    err => {
-      if (err) throw err;
-      let commandStr = "SELECT * FROM users WHERE userId = ?";
+  let dbRowID = 1; // temporary! Should be the real unique
+  // key for db Row for this user in DB table.
+  // Note: cannot be zero, has to be something that evaluates to
+  // True.
 
-      database.get(commandStr, [profile.id], (err, rows) => {
-        // console.log("looking for userId = " + profile.id + " in users");
-        if (err) throw err;
-        if (!rows) {
-          // console.log("user doesn't exist!");
-          database.serialize(() => {
-            database.run(
-              `INSERT INTO users (
-              userId,
-              firstName,
-              lastName
-          )
-          VALUES (?, ?, ?);`,
-              [profile.id, profile.name.givenName, profile.name.familyName]
-            );
-          });
-        } else {
-          // console.log("user exists!");
-        }
-      });
-
-      let dbRowID = profile.id;
-
-      done(null, dbRowID);
-    }
-  );
+  done(null, dbRowID);
 }
 
 // Part of Server's sesssion set-up.
@@ -329,23 +193,23 @@ passport.serializeUser((dbRowID, done) => {
 // Whatever we pass in the "done" callback becomes req.user
 // and can be used by subsequent middleware.
 passport.deserializeUser((dbRowID, done) => {
-
-  console.log("DeserializeUser. Input is:", dbRowID);
-
+  console.log("deserializeUser. Input is:", dbRowID);
   // here is a good place to look up user data in database using
   // dbRowID. Put whatever you want into an object. It ends up
   // as the property "user" of the "req" object.
+  let userData = { userData: "data from db row goes here" };
+  done(null, userData);
+});
 
-  let userData;
-
-  let sqlMesssage = `SELECT * FROM users WHERE userId = ?`;
-  database.get(sqlMesssage, [dbRowID], (err, row) => {
-    if (err) {
-      console.log(err);
-      return done(null, dbRowID);
-    } else {
-      userData = row;
-      done(null, userData);
-    }
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
+
+module.exports = app;
