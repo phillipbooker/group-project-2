be2efcb01404a92dc00b079f5e435c0e9a07c3b9
rasenin <romansenin15@gmail.com>
2019-06-29 16:02:38 -0700

    included passport setup/package to needed files

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 // auth login
 router.get("/login", (req, res) => {
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 const express = require("express");
 const exphbs = require("express-handlebars");
 
+// Setup passport configurations
+require("./config/passport-setup");
+
 const db = require("./models");
 
 const app = express();
