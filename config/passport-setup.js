a929f5ce5ee1ac21c9825af2def168aeb0a6c1f3
rasenin <romansenin15@gmail.com>
2019-06-30 03:15:25 -0700

    added serialize and deserializer functions

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,16 +1,42 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
 
+const db = require("../models");
+
+const googleRedirect = process.env.PORT
+  ? "/auth/google/redirect"
+  : "http://localhost:3000/auth/google/redirect";
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((userId, done) => {
+  db.User.findOne({ where: { id: userId } }).then(user => {
+    console.log("result of findOne: ", user);
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
       // options for the google strategy
       clientID: process.env.clientID,
       clientSecret: process.env.clientSecret,
-      callbackURL: "auth/google/redirect"
+      callbackURL: googleRedirect
     },
-    () => {
+    (accessToken, refreshToken, profile, done) => {
       // passport callback function
+      console.log(profile);
+      db.User.findOrCreate({
+        where: { gid: profile.id, gname: profile.displayName }
+      }).then(([user, created]) => {
+        let userData = user.get({ plain: true });
+        console.log(userData);
+        console.log("created: ", created);
+        return done(null, userData);
+      });
     }
   )
 );
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -27,7 +27,7 @@ router.get(
 );
 
 // callback route for google to redirect to
-router.get("/google/redirect", (req, res) => {
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
   res.send("You reached the callback URI");
 });
 
