bb786a0484962c66e424c89588af3fa3b2fc32b2
rasenin <romansenin15@gmail.com>
2019-06-30 03:50:56 -0700

    changed router var to an exported function

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,34 +1,32 @@
-const express = require("express");
-const router = express.Router();
 const passport = require("passport");
 
-// auth login
-router.get("/auth/login", (req, res) => {
-  res.render("login");
-});
+module.exports = function(app) {
+  // auth login
+  app.get("/auth/login", (req, res) => {
+    res.render("login");
+  });
 
-// auth logout
-router.get("/auth/logout", (req, res) => {
-  // handle with passport
-  res.send("logging out");
-});
+  // auth logout
+  app.get("/auth/logout", (req, res) => {
+    // handle with passport
+    res.send("logging out");
+  });
 
-// auth with google
-router.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile"],
-    prompt: "select_account"
-  })
-);
+  // auth with google
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile"],
+      prompt: "select_account"
+    })
+  );
 
-// callback route for google to redirect to
-router.get(
-  "/auth/google/redirect",
-  passport.authenticate("google"),
-  (req, res) => {
-    res.send("You reached the callback URI");
-  }
-);
-
-module.exports = router;
+  // callback route for google to redirect to
+  app.get(
+    "/auth/google/redirect",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.send("You reached the callback URI");
+    }
+  );
+};
