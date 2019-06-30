c55034d2599181273b46ce685c542ce0e8fbe5e0
rasenin <romansenin15@gmail.com>
2019-06-30 03:44:26 -0700

    added cookie-session and intialized passport

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 
+// Load passport configurations
+require("./config/passport-setup");
+
 // Globals
 const express = require("express");
 const exphbs = require("express-handlebars");
-
-// Setup passport configurations
-require("./config/passport-setup");
+const cookieSession = require("cookie-session");
 
 const db = require("./models");
 
@@ -16,6 +17,14 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000, // 24-hour session
+    keys: [process.env.cookieKey]
+  })
+);
+app.use(passport.intitialize());
+app.use(passport.session());
 
 // Handlebars
 app.engine(
