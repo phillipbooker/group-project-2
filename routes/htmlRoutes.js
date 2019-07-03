023bc83006cddb9d25c5a60ccaae34ec813c56a8
Phillip Booker <phillippbooker@gmail.com>
2019-07-02 23:27:38 -0700

    Removes examples query in index.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,23 +2,8 @@ var db = require("../models");
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples,
-        style: "landing.css"
-      });
-    });
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
+    res.render("index", {
+      style: "landing.css"
     });
   });
 
