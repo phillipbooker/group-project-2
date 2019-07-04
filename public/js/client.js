1ca318c9e4ec3af1e98ba572297f59e1b1b38690
Roman <romansenin15@gmail.com>
2019-07-04 12:45:24 -0700

    fixed card item css

--- public/js/client.js
+++
@@ -1,7 +0,0 @@
-function readMore() {
-  var expandInfo = document.getElementById("more-info-js");
-  var mainHeadings = document.getElementById("main-headings-js");
-
-  mainHeadings.style.transform = "scale(0.7)";
-  expandInfo.style.height = "350px";
-}
