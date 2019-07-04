2520492bec9eeef0237b701f3be4b6c74cfa29c6
Phillip Booker <phillippbooker@gmail.com>
2019-07-04 12:07:09 -0700

    Adds scrolling to form on stylist page.

--- public/js/stylist.js
+++ public/js/stylist.js
@@ -128,3 +128,24 @@ $deleteButton.on("click", function(e) {
     location.reload();
   });
 });
+
+$("a[href^='#']").on("click", function(e) {
+  // prevent default anchor click behavior
+  e.preventDefault();
+
+  // store hash
+  var hash = this.hash;
+
+  // animate
+  $("html, body").animate(
+    {
+      scrollTop: $(hash).offset().top
+    },
+    1000,
+    function() {
+      // when done, add hash to url
+      // (default click behaviour)
+      window.location.hash = hash;
+    }
+  );
+});
