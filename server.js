6109acb7f37be4531eb644ecd773ced8ddefa2a5
Roman <romansenin15@gmail.com>
2019-07-05 10:53:10 -0700

    removed browser cache access after logout

--- server.js
+++ server.js
@@ -30,6 +30,14 @@ app.use(passport.session());
 if (process.env.PORT) {
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
 }
+// caching disabled for every route
+app.use(function(req, res, next) {
+  res.set(
+    "Cache-Control",
+    "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+  );
+  next();
+});
 
 // Handlebars
 app.engine(
